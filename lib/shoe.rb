09bfad18c7a487a904c84cd624874bde56515636
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-24 03:30:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,27 @@
-class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+describe 'Shoe' do 
+  describe 'BRANDS' do
+    # Think about it! When does the BRANDS constant need to be
+    # made aware of the brands of the shoes that are being created?
 
-  def initialize(brand)
-    @brand = brand
+    it 'keeps track of all brands' do
+      brands = ["Uggs", "Rainbow"]
+      brands.each do |brand|
+        Shoe.new(brand)
+      end
+
+      brands.each do |brand|
+        expect(Shoe::BRANDS).to include(brand)
+      end
+    end
+
+    it 'only keeps track of unique brands' do
+      Shoe::BRANDS.clear
+      brands = ["Uggs", "Rainbow", "Nike", "Nike"]
+      brands.each do |brand|
+        Shoe.new(brand)
+      end
+
+      expect(Shoe::BRANDS.size).to eq(3)
+    end
   end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-
 end
